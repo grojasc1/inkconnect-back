95d58ceafbe0055d11f627f40d752c272d3b66ee
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-12-20 12:43:10 -0500

    add user entity and module for user management

---
+++ src/user/user.entity/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    created_at: Date;
+}
