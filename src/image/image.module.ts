cd2cf8d220b924a01a669e4c776251e69d87b0ff
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-12-26 15:34:02 -0500

    add ImageModule and ImageService with TypeORM integration and unit tests

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ImageService } from './image.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ImageEntity } from './image.entity/image.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ImageEntity])],
+  providers: [ImageService]
+})
 export class ImageModule {}
